7ee79d3476d2f89ba3881bb2eb9bdee3677c6646
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2021-04-08 11:00:10 +0500

    Initial Commit

---
+++ TestTaskStroy/TestTaskStroyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskStroyApp.swift
+//  TestTaskStroy
+//
+//  Created by Сергей on 08.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskStroyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
